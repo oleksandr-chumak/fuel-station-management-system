6faee77585ebbb3c2a7a9ad3857f5a9af574d4cb
Oleksandr Chumak <oleksandr.chumak.anatoliiovych@gmail.com>
2025-04-28 16:24:17 +0200

    Implement 'me' endpoint

--- rest-api/src/main/java/com/fuelstation/managmentapi/authentication/application/AuthController.java
+++ rest-api/src/main/java/com/fuelstation/managmentapi/authentication/application/AuthController.java
@@ -1,13 +1,19 @@
 package com.fuelstation.managmentapi.authentication.application;
 
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fuelstation.managmentapi.authentication.domain.Credentials;
+import com.fuelstation.managmentapi.authentication.domain.CredentialsRepository;
 import com.fuelstation.managmentapi.authentication.domain.UserRole;
 import com.fuelstation.managmentapi.authentication.infrastructure.services.AuthenticationService;
 
@@ -17,6 +23,9 @@ public class AuthController {
 
     @Autowired
     private AuthenticationService authService;
+
+    @Autowired
+    private CredentialsRepository credentialsRepository;
 
     @PostMapping("/login/admin") 
     public ResponseEntity<String> loginAdmin(@RequestBody AuthRequest authRequest) {
@@ -28,5 +37,12 @@ public class AuthController {
     public ResponseEntity<String> loginManager(@RequestBody AuthRequest authRequest) {
         String accessToken = authService.authenticate(authRequest.getEmail(), authRequest.getPassword(), UserRole.Manager);
         return new ResponseEntity<String>(accessToken, HttpStatus.OK);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<CredentialsResponse> getCurrentUser(Authentication authentication) {
+        Credentials credentials = credentialsRepository.findByEmail(authentication.getName())
+            .orElseThrow(() -> new NoSuchElementException("User doesn't exist"));
+        return new ResponseEntity<>(CredentialsResponse.fromDomain(credentials), HttpStatus.OK);
     }
 }
---
+++ rest-api/src/main/java/com/fuelstation/managmentapi/authentication/application/CredentialsResponse.java
@@ -0,0 +1,21 @@
+package com.fuelstation.managmentapi.authentication.application;
+
+import com.fuelstation.managmentapi.authentication.domain.Credentials;
+import com.fuelstation.managmentapi.authentication.domain.UserRole;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CredentialsResponse {
+    private String email; 
+    private UserRole userRole; 
+
+    public static CredentialsResponse fromDomain(Credentials credentials) {
+        return new CredentialsResponse(credentials.getEmail(), credentials.getRole());
+    }
+
+}
